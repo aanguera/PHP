f888ba1857aa98865b7c736a4441ffad2263debf
root <root@raspberrypi.(none)>
2016-02-18 23:44:54 +0100

    first string1.php

---
+++ strings1.php
@@ -0,0 +1,19 @@
+<html>
+   <head>
+      <title>Online PHP Script Execution</title>
+   </head>
+   
+   <body>
+		
+      <?php
+         echo "<h1>Hello, PHP!</h1>";
+	 		echo "<p>I'm using PHP</>";
+	 		echo '<p>This is "my first" program</p>';
+			echo '<p>I\'m using backslash in this line</p>';
+			echo "I'm using 4 backslash for printing only 2 \\\\ backslash";
+	 		echo "<h3>Bye !<h3>";
+		// End of the messages
+      ?>
+      
+   </body>
+</html>
