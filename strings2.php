8b7f85dffd38cfbce34210f0f96c7ed62ed5ee27
root <root@raspberrypi.(none)>
2016-02-18 23:46:29 +0100

    first string2.php

---
+++ strings2.php
@@ -0,0 +1,17 @@
+<html>
+   <head>
+      <title>Strings2</title>
+   </head>
+   
+   <body>
+		
+      <?php
+      	$name = 'Toni';
+         echo "<h1>Hello, $name</h1>";
+	 		echo '$name This line uses single quotation.';
+			echo "<p>But in this case, $name the string have a double quotation</p>";
+			// End of the messages
+      ?>
+      
+   </body>
+</html>
