00f9ed5e5fd7ab29627f80b6265b4e0d18a06184
root <root@raspberrypi.(none)>
2016-02-25 18:34:58 +0100

    adding the new file operators2.php

--- operators2.php
+++ operators2.php
@@ -19,6 +19,15 @@
          echo $num *=2;      
          echo "<br>Now the assignement is <b>\$num %=2</b> and the num's value is: ";
          echo $num %=2;
+         
+         // This section assign vatiables by reference
+         echo "<h1>Assign by reference</h1>";
+         $boy = 15;
+         $girl = &$boy;
+         echo "The initial variable's value of \$boy is '15' and \$girl is assigned by reference to \$boy <br>";
+         $boy +=3;
+         echo "Then, the value of \$girl after \$boy +=3, is: $girl"; 
+         
       ?>
       
    </body>
