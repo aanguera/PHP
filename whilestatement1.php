309f5e6f0b78709d3919f6da48ac6e9eae736d4b
root <root@raspberrypi.(none)>
2016-03-05 17:38:25 +0100

    Adding the loop do while to the file whilestatement1.php

--- whilestatement1.php
+++ whilestatement1.php
@@ -7,7 +7,7 @@
 		
       <?php
       	// This program looks for a name inside an array
-         echo "<h1>While and if statement</h1>";
+         echo "<h1>While, do while and if statement</h1>";
 	 		$users = array("John", "Huang", "Douglas", "Tony", "Charles", "Smith", "George");
 	 		$testvar = "no";
 	 		$k = 0;
@@ -21,6 +21,14 @@
 	 			}
 	 			$k++;
 	 		}
+	 		echo "<h2> Do while statement</h2>";
+	 		echo "The loop condition is <= 5, but we can see 6 because it's a do while loop<br>";
+			echo "Firts it executes the loop, then it test the condition.<br>";	 		
+	 		$k = 6;
+	 		do {
+	 			echo "The index number is: <b>$k</b><br>";
+	 			$k++;
+	 		} while ($k <= 5);
 		?>
       
    </body>
