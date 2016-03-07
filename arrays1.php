ad64967154e08218b11759d81bdc48d69ff60fd5
root <root@raspberrypi.(none)>
2016-03-07 07:47:03 +0100

    Adding statements to  arrays1.php, for adding new elements and overwrite others elements

--- arrays1.php
+++ arrays1.php
@@ -16,6 +16,14 @@
 	 			echo "The index <b>$k</b> is the user: <b>$users[$k]</b><br>";
 	 			$k++;	
 	 		}
+	 		echo "<h3>Adding new elements</h3>";
+	 		echo "The sentence <b>\$users [] = \"Michael\";</b>,  add the new element: <i>Michael</i><br>";
+	 		$users [] = "Michael";
+	 		$k = 0;	 		
+	 		while ($k<sizeof($users)){
+	 			echo "The index <b>$k</b> is the user: <b>$users[$k]</b><br>";
+	 			$k++;	
+	 		}
 	 		echo "<h2>This is an associative array printed by \"print_r\"</h2>";
 	 		echo "<h3>Printed by \"print_r\"</h3>";
 	 		print_r($capitals);
@@ -23,7 +31,11 @@
 			echo "In this case using the instruction <b>\$capitals['GE'], </b>print the value: <b> ";	
 			echo $capitals['GE']; 		
 			echo "</b>";
-	 		 
+			echo "<h3>Overwrite elements</h3>";
+			echo "We can overwrite with the sentence <b>\$capitals ['FR'] = \"Paris-France\";</b><br>";
+			$capitals['FR'] = "Paris-France";
+			echo "Now the array looks like:<br>";
+			print_r($capitals);	 		 
 		?>
       
    </body>
