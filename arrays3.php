ed99879dd30e9306a7a3ddfb39e3312a3b6f9204
root <root@raspberrypi.(none)>
2016-03-20 18:23:24 +0100

    I Add the printing system

--- arrays3.php
+++ arrays3.php
@@ -10,12 +10,11 @@
          echo "<h1>Multidimensional array</h1>";
          $food = array(
          	'fruits' => array('banana', 'orange', 'apple'),
-         	'dark_green_vegetables' => array('spinach', 'swiss chard'),
+         	'dark green vegetables' => array('spinach', 'swiss chard'),
+         	'orange vegetables' => array('pumpkin', 'carots'),
          );
 	 		
-			echo "<h3>In this case we use it to print all the array content</h3>";			
-			echo $food[0][1]."<br>";
-#			print_r($food);
+			echo "<h3>This is a associative and index multidimensional array</h3>";			
 			echo "<ul>";			
 			foreach ($food as $key => $value){
 				echo "<li>$key";
@@ -23,20 +22,10 @@
   				for ($col = 0; $col < sizeof($food[$key]); $col++) {
     				echo "<li>".$food[$key][$col]."</li>";
   				}
-  				echo "</ul>";		
+  				echo "</ul><br>";		
 			}			
 			echo "</ul>";
-			$k = 0;	 		
-	 		while ($k<sizeof($food)){
-	 			echo "The index <b>$k</b><br>";
-	 			echo "<ul>";
-  				for ($col = 0; $col < 2; $col++) {
-    				echo "<li>".$food[$k][$col]."</li>";
-  				}
-  				echo "</ul>";
-	 			$k++;	
-	 		}
-	 			 		
+				 			 		
 		?>
       
    </body>
