9716dab48b6044e4b01177414e91649a74aa8a6f
root <root@raspberrypi.(none)>
2016-03-24 09:21:08 +0100

    Adding the new program for testing functions

---
+++ functions1.php
@@ -0,0 +1,19 @@
+<html>
+   <head>
+      <title>Functions1</title>
+   </head>
+   
+   <body>
+		
+      <?php
+      	// This program uses functions
+         echo "<h1>Functions</h1>";
+         function first(){
+         	echo "Hello this is the first function without parameters";
+         }
+			echo "We call the first function with no argument<br>";
+			first();
+		?>
+      
+   </body>
+</html>
