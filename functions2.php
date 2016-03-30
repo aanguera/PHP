c245a8dfc413570ed13112ecf183cb7cbb7e43d8
root <root@raspberrypi.(none)>
2016-03-30 23:07:57 +0200

    Adding the function function2.php for using as included function

---
+++ functions2.php
@@ -0,0 +1,19 @@
+<html>
+   <head>
+      <?php
+      	$title_name="Functions2"; 
+	      include('includes/title.php'); 
+      ?>
+   </head>
+   
+   <body>
+		
+      <?php
+      	// This program uses include functions
+         echo "<h1>Functions</h1>";
+         echo "<h2>Using include functions</h2>";
+         			
+		?>
+      
+   </body>
+</html>
