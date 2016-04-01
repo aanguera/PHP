9ce3d12c3a84cbeef95e0b9cb00c69ce08297d89
root <root@raspberrypi.(none)>
2016-04-01 20:17:17 +0200

    Adding the php script for managing html form

---
+++ handle_form.php
@@ -0,0 +1,16 @@
+<html>
+   <head>
+      <title>Basic form result</title>
+   </head>
+   
+   <body>
+		
+      <?php
+      	// This program shows the content's form
+         echo "<h1>Basic form</h1>";
+         echo "<h3>Hello Mr.</h3>";
+         echo $_POST["name"];			
+		?>
+      
+   </body>
+</html>
