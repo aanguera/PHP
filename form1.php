d11bddd2d5efba5b0ead520b32ee458fb307a961
root <root@raspberrypi.(none)>
2016-04-01 20:15:41 +0200

    Adding a basic form

---
+++ form1.php
@@ -0,0 +1,21 @@
+<html>
+   <head>
+      <title>Basic form</title>
+   </head>
+   
+   <body>
+		<h1>Basic form</h1>
+		<form action="handle_form.php" method="post">
+		<fieldset>
+			<legend>Enter your name !</legend>
+			<p><label>Name: <input type="text" name="name" size="30" maxlength="50" </label></p>		
+		</fieldset>
+		<p><input type="submit" name="submit" value="Submit for testing"</p>
+		
+		</form>
+      <?php
+      	                  			
+		?>
+      
+   </body>
+</html>
