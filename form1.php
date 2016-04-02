03246772230c332c2c04bc79a0eb7abc0f53a345
root <root@raspberrypi.(none)>
2016-04-02 16:06:37 +0200

    Adding radio inputs at the form

--- form1.php
+++ form1.php
@@ -7,8 +7,12 @@
 		<h1>Basic form</h1>
 		<form action="handle_form.php" method="post">
 		<fieldset>
-			<legend>Enter your name !</legend>
-			<p><label>Name: <input type="text" name="name" size="30" maxlength="50" </label></p>		
+			<legend>Enter your name</legend>
+			<p><label>Name: <input type="text" name="name" size="30" maxlength="50" </label></p>
+			<p><label for="type">Gender</label>
+			<input type="radio" name="gender" value="M" /> Male
+			<input type="radio" name="gender" value="F" /> Female
+			</p>		
 		</fieldset>
 		<p><input type="submit" name="submit" value="Submit for testing"</p>
 		
