72eaff6edc374ba0aff86eaf8d3258d1f6b1c96d
root <root@raspberrypi.(none)>
2016-04-07 20:54:37 +0200

    Adding the aboutme file

---
+++ aboutme.php
@@ -0,0 +1,9 @@
+<?php
+	$page_tittle = 'Main page';
+	include('includes/header.html');
+?>
+<p><strong>About me</strong></p>
+<p>Phone number: 12345678</p>
+<?php
+	include('includes/footer.html');
+?>
