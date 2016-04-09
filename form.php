e0c4da14097ae9333077a45752f26c8517cbc836
root <root@raspberrypi.(none)>
2016-04-09 18:32:41 +0200

    Adding the file form.php

---
+++ form.php
@@ -0,0 +1,9 @@
+<?php
+	$page_tittle = 'News letter signup';
+	include('includes/header.html');
+?>
+<p><strong>Content here.</strong></p>
+<p>Sapibulumnibh phasellus nulla egestibulum enim pretium elit tincidunt estiquam ultrisque donectetur. Sedcondimentumsan odio hendrerit proin vitae dignis nibh ac justo id congue. Amesintesque vel curabitae volutpat donec alique nasceleifendimentesque montesque rhoncus quis eros. Vestnunc nonummy</p>
+<?php
+	include('includes/footer.html');
+?>
