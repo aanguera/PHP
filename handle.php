562b2f22fe2f4760a20659e2609a4734f480dd77
root <root@raspberrypi.(none)>
2016-04-11 07:46:45 +0200

    Adding more opotions for testing the radio button of sex

--- handle.php
+++ handle.php
@@ -17,12 +17,23 @@ if(!empty($_REQUEST['email'])) {
 
 if(isset($_REQUEST['sex'])) {
 	$sex = $_REQUEST['sex'];
+
+	if($sex == "male") {
+		echo "Hello Sir";
+	}elseif($sex == "female") { //It's for safety reasons, because it's only available two options
+		echo "Hello Madam";
+	}else {
+		echo "Enter the correct value";
+	}
+	echo "<br>";
+	
 }else {
 	$sex = NULL;
 }
 
 if($name && $email && $sex) {
-	echo "We will email you shortly!";
+	echo "I'm glad you are interested in our newsletter. Here is a custom message from Toni: <br>";
+	echo "Thank you, $name. We will email you shortly at $email !";
 }else {
 	echo 'Please go back and try the form again by clicking <a href="newsletter.php">Here</a>';
 }
