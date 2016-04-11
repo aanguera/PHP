991f4a8ca029c367ef7419b2c0245786747244e7
root <root@raspberrypi.(none)>
2016-04-11 07:48:43 +0200

    Changing the old values rarely and never for the sex variable coorect options male and female

--- newsletter.php
+++ newsletter.php
@@ -6,8 +6,8 @@
 	<fieldset><legend>Enter your information below</legend>
 	<p><label>Name: <input type="text" name="name" size="30" maxlength="50"</label></p>
 	<p><label>Email: <input type="text" name="email" size="30" maxlength="50"</label></p>
-	<p><label for="sex">Sex: </label> <input type="radio" name="sex" value="rarely"> Rarely 
-	<input type="radio" name="sex" value="never"> Never</p>	
+	<p><label for="sex">Sex: </label> <input type="radio" name="sex" value="male"> Male 
+	<input type="radio" name="sex" value="female"> Female</p>	
 	</fieldset>
 	<p align="center"><input type="submit" name="submit" value="Submit button"</p>
 	</form>
